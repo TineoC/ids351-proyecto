ddad0d688eecd592af4399dee05307543f245bf6
AlexBujosa <bujosa2014@outlook.com>
2023-04-03 18:10:40 -0400

    feat: :sparkles: client http request

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,21 @@ import { PrismaClient } from '@prisma/client'
 
 const app: Express = express()
 const port = 3000
+app.use(express.json())
+app.use(express.urlencoded())
 
 const prisma = new PrismaClient()
 
+app.get('/', (req: Request, res: Response) => {
+  res.send('Klk')
+})
+
 app.post('/products', async (req, res) => {
+  console.log(req.body)
   const product = await prisma.products.create({
     data: req.body,
   })
-  res.json(product)
-})
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Klk')
+  res.json({ product })
 })
 
 app.listen(port, () => {
