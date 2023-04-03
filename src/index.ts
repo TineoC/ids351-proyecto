3221114b9c3e6f787f528b5dc8e1002175f5e6fa
AlexBujosa <bujosa2014@outlook.com>
2023-04-03 17:08:08 -0400

    feat: :sparkles: product create endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
 import express, { type Express, type Request, type Response } from 'express'
 
+import { PrismaClient } from '@prisma/client'
+
 const app: Express = express()
 const port = 3000
+
+const prisma = new PrismaClient()
+
+app.post('/products', async (req, res) => {
+  const product = await prisma.products.create({
+    data: req.body,
+  })
+  res.json(product)
+})
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Klk')
